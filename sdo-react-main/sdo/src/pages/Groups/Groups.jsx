6ddc0421e3a15b1f410fcf40e88444618e57b6ca
qpfilw <125376171+qpfilw@users.noreply.github.com>
2024-10-28 04:55:16 +0300

    add groups

---
+++ sdo-react-main/sdo/src/pages/Groups/Groups.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import styled from "styled-components";
+
+import GroupWindow from "./components/GroupWindow.jsx";
+
+const GroupsContainer = styled.div`
+  display: flex;
+
+  flex-direction: column; /* Располагаем дочерние элементы в столбик */
+
+  align-items: center; /* Центрирование по горизонтали */
+
+  margin: 20px; /* Отступ сверху и центрирование по горизонтали */
+`;
+
+const StyledContainer = styled.div`
+  background-color: #e6f4cf;
+
+  font-family: "Montserrat";
+
+  width: 1480px;
+
+  height: 100px;
+
+  padding: 0 20px;
+
+  display: flex;
+
+  align-items: center;
+
+  justify-content: space-between;
+
+  margin: 10px 0; /* Отступ между компонентами */
+
+  transition: background-color 0.3s;
+
+  border-radius: 7px;
+`;
+
+const GroupContent = styled.div`
+  display: flex;
+
+  align-items: center;
+`;
+
+const GroupNumber = styled.div`
+  font-weight: normal;
+
+  font-size: 20px;
+
+  margin-right: 10px;
+`;
+
+const ChangeButton = styled.button`
+  font-weight: normal;
+
+  font-size: 20px;
+
+  background-color: #becbee;
+
+  color: white;
+
+  border: none;
+
+  border-radius: 5px;
+
+  cursor: pointer;
+
+  height: 42px;
+
+  padding: 0 15px;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+const Groups = () => {
+  const groupNumbers = ["221-375", "221-376", "221-377"];
+
+  return (
+    <GroupsContainer>
+      {groupNumbers.map((number, index) => (
+        <GroupWindow key={index} groupNumber={number} />
+      ))}
+    </GroupsContainer>
+  );
+};
+
+export default Groups;
