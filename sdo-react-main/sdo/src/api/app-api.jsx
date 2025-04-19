5370c4d01be421d649181fc9034911013984aa82
None <None>
2025-04-19 22:08:28 +0300

    update

--- sdo-react-main/sdo/src/api/app-api.jsx
+++ sdo-react-main/sdo/src/api/app-api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const appApiIns = axios.create(
     {
-        baseURL : 'http://127.0.0.1:8000/',
+        baseURL : 'http://147.45.232.120:8000/',
         headers :{
             'Content-Type' : 'application/json',
             'Authorization' : 'Bearer ' + localStorage.getItem('access_token') 
